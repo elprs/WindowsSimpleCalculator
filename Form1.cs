fc38caac59e454210f91f55741834b6999531f0a
elprs <eleni.parisi.sunshine@gmail.com>
2020-01-18 16:25:31 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsSimpleCalculator
+{
+    public partial class Form1 : Form
+    {
+        double value = 0;
+        string operation = "";
+        bool isOperationPressed = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if((result.Text == "0") || (isOperationPressed))
+            {
+                result.Clear();
+            }
+            Button b = (Button)sender;
+            result.Text = result.Text + b.Text;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            result.Text = "0";
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            operation = b.Text;
+            value = double.Parse(result.Text);
+            isOperationPressed = true;
+        }
+    }
+}
